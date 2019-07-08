52a09bf1d9a97aefae7a190675758bc5854f4afe
Julien <caron-julien@live.fr>
2019-07-08 15:26:47 +0200

    setup redux minimal implementation && little src/ refactoring

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../styles/App.css'
+
+const App = () => (
+  <main className="App">
+    <header>
+      <p>
+        Edit <code>src/App.jsx</code> and save to reload.
+      </p>
+    </header>
+  </main>
+)
+
+export default App
